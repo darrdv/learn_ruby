abcc7a5aad356bb460cd33ce0a658d79b7171285
Darren.DV <darr.dv@gmail.com>
2016-08-16 18:11:27 -0400

    In progress

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,16 @@
+def echo(say)
+  "\"#{say}\""
+end
+
+def shout(say)
+  "\"#{say.upcase}\""
+end
+
+def repeat(string, num = 2)
+  puts "\""
+  num.times do |i|
+  puts "#{i} "
+  puts "\""
+  end
+ end
+# end
