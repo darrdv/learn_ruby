bde60660259029cfd65b6af91a7019f64f3c2e94
Darren.DV <darr.dv@gmail.com>
2016-08-17 14:08:07 -0400

    More errors corrected

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,16 +1,41 @@
 def echo(say)
-  "\"#{say}\""
+  "#{say}"
 end
 
 def shout(say)
-  "\"#{say.upcase}\""
+  "#{say.upcase}"
 end
 
 def repeat(string, num = 2)
-  puts "\""
-  num.times do |i|
-  puts "#{i} "
-  puts "\""
+  return ( [string] * num ).join(' ')
+end
+
+def titleize(string)
+  title = string.split
+  title.each do |word|
+    if word.include?("and")
+      word.downcase!
+    elsif word.include?("the")
+      word.downcase!
+    elsif word.include?("over")
+    elsif word.length
+      word.capitalize!
+    end
   end
- end
-# end
+
+  title[0] = title[0].capitalize
+  new_title = title.join(" ")
+  new_title.rstrip
+  return new_title
+
+end
+
+def first_word(string)
+  words = string.split(' ')
+  return words[0]
+end
+
+def start_of_word(word, num_letter)
+  word.split("")
+  return word[0..(num_letter - 1)]
+end
